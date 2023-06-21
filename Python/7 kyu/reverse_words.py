ef58e616e0446fb7e3851754a52392e15e3cf58a
ruipinto <ruifspinto@gmail.com>
2023-06-21 20:10:03 +0100

    personal kata

--- Python/7 kyu/reverse_words.py
+++ Python/7 kyu/reverse_words.py
@@ -1,8 +1,3 @@
-import os
-
-import pyttsx3
-
-
 def reverse_words(string: str) -> str:
     """
     Reverses the order of words in a given string.
@@ -16,21 +11,6 @@ def reverse_words(string: str) -> str:
     return ' '.join([word[::-1] for word in string.split()])
 
 
-# Initialize the text-to-speech engine
-engine = pyttsx3.init()
-
-# Test the function
-input_string = input("Enter a string: ")
-reversed_string = reverse_words(input_string)
-print("Reversed string:", reversed_string)
-
-# Speak the reversed string
-engine.say(reversed_string)
-
-# Save the output audio to a file
-output_file = "output.wav"
-engine.save_to_file(reversed_string, output_file)
-engine.runAndWait()
-
-# Provide the output file path
-print("Output file saved:", os.path.abspath(output_file))
+if __name__ == '__main__':
+    assert reverse_words('Hello World!') == 'olleH !dlroW'
+    print('All tests passed successfully!')
