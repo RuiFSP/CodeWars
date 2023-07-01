6b8b363dd8e2393eab5c1c509b4629fefff34ee7
Ruifspinto <ruifspinto@ruis-MacBook-Pro.local>
2023-07-01 21:17:27 +0100

    solved one kyu8

---
+++ Python/8 kyu/amount_of_points.py
@@ -0,0 +1,10 @@
+def points(games):
+    # my code
+    result = 0
+    for i in games:
+        j = i.split(':')
+        if j[0] > j[1]:
+            result+=3
+        elif j[0] == j[1]:
+            result += 1
+    return result
