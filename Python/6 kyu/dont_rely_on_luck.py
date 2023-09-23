efa623ba81e7ec39b4d25caa6ee54b3aa54c4fe7
ruifspinto <ruifspinto@gmail.com>
2023-09-23 11:17:41 +0100

    dont rely on luck - kata6

---
+++ Python/6 kyu/dont_rely_on_luck.py
@@ -0,0 +1,18 @@
+from random import randint, seed
+
+# The test fixture I use for this kata is pre-populated.
+
+# It will compare your guess to a random number generated using:
+
+# randint(1,100)
+
+# You can pass by relying on luck or skill but try not to rely on luck.
+
+# "The power to define the situation is the ultimate power." - Jerry Rubin
+
+# Good luck!
+
+
+seed(1)
+guess = randint(1,100)
+seed(1)
