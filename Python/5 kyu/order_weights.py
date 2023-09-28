c17136428b07d678076a5600b24d062639adaf0a
rui pinto <ruifspinto@gmail.com>
2023-09-28 11:47:47 +0100

    solved kata6 tic-tac-toe

--- Python/5 kyu/order_weights.py
+++ Python/5 kyu/order_weights.py
@@ -1,10 +1,10 @@
 import unittest
 
-""" 
-My friend John and I are members of the "Fat to Fit Club (FFC)". John is worried because each month a list with 
+"""
+My friend John and I are members of the "Fat to Fit Club (FFC)". John is worried because each month a list with
 the weights of members is published and each month he is the last on the list which means he is the heaviest.
 
-I am the one who establishes the list so I told him: "Don't worry any more, I will modify the order of the list". 
+I am the one who establishes the list so I told him: "Don't worry any more, I will modify the order of the list".
 It was decided to attribute a "weight" to numbers. The weight of a number will be from now on the sum of its digits.
 
 For example 99 will have "weight" 18, 100 will have "weight" 1 so in the list 100 will come before 99.
@@ -12,7 +12,7 @@ For example 99 will have "weight" 18, 100 will have "weight" 1 so in the list 10
 Given a string with the weights of FFC members in normal order can you give this string ordered by "weights" of these numbers?
 Example:
 
-"56 65 74 100 99 68 86 180 90" ordered by numbers weights becomes: 
+"56 65 74 100 99 68 86 180 90" ordered by numbers weights becomes:
 
 "100 180 90 56 65 74 68 86 99"
 
@@ -22,11 +22,11 @@ When two numbers have the same "weight", let us class them as if they were strin
 
 All numbers in the list are positive numbers and the list can be empty. """
 
-def order_weight(strng):
+def order_weight(str:str) -> str:
     def weight_key(number):
         return (sum(int(digit) for digit in number), number)
 
-    numbers = strng.split()
+    numbers = str.split()
     sorted_numbers = sorted(numbers, key=weight_key)
 
     return ' '.join(sorted_numbers)
@@ -39,4 +39,3 @@ class TestOrderWeight(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
---
+++ Python/6 kyu/tic_tac_toe_table.py
@@ -0,0 +1,48 @@
+"""
+Do you have in mind the good old TicTacToe?
+
+Assuming that you get all the data in one array, you put a space around each
+value, | as a columns separator and multiple - as rows separator, with something
+like ["O", "X", " ", " ", "X", " ", "X", "O", " "] you should be returning this
+structure (inclusive of new lines):
+
+ O | X |
+-----------
+   | X |
+-----------
+ X | O |
+
+Now, to spice up things a bit, we are going to expand our board well beyond a
+trivial 3 x 3 square and we will accept rectangles of big sizes, still all as a
+long linear array.
+
+For example, for "O", "X", " ", " ", "X", " ", "X", "O", " ", "O"] (same as
+above, just one extra "O") and knowing that the length of each row is 5, you
+will be returning
+
+ O | X |   |   | X
+-------------------
+   | X | O |   | O
+
+And worry not about missing elements, as the array/list/vector length is always
+going to be a multiple of the width.
+ """
+
+def display_board(board:list, width:int) -> str:
+    row_strings = []
+    for i in range(0, len(board), width):
+        row = board[i:i + width]
+        row_str = " | ".join(row)
+        row_strings.append(f" {row_str} ")
+
+        if i < len(board) - width:  # Check if it's not the last row
+            row_strings.append("-" * (len(row_strings[0])))
+
+    return '\n'.join(row_strings)
+
+if __name__ == "__main__":
+    assert(display_board(["O", "X", "X", "O"],2) == " O | X \n-------\n X | O ")
+    assert(display_board(["O", "X", " ", " ", "X", " ", "X", "O", " "],3) == " O | X |   \n-----------\n   | X |   \n-----------\n X | O |   ")
+    assert(display_board(["O", "X", " ", " ", "X", " ", "X", "O", " ", "O"],5) == " O | X |   |   | X \n-------------------\n   | X | O |   | O ")
+    assert(display_board(["O", "X", " ", " ", "X", " ", "X", "O", " ", "O"],2) == " O | X \n-------\n   |   \n-------\n X |   \n-------\n X | O \n-------\n   | O ")
+    assert(display_board(["1", "2", "3", "4", "5", "1", "2", "3", "4", "5", "1", "2", "3", "4", "5", "1", "2", "3", "4", "5", "1", "2", "3", "4", "5", "1", "2", "3", "4", "5", "1", "2", "3", "4", "5", "1"],6) == " 1 | 2 | 3 | 4 | 5 | 1 \n-----------------------\n 2 | 3 | 4 | 5 | 1 | 2 \n-----------------------\n 3 | 4 | 5 | 1 | 2 | 3 \n-----------------------\n 4 | 5 | 1 | 2 | 3 | 4 \n-----------------------\n 5 | 1 | 2 | 3 | 4 | 5 \n-----------------------\n 1 | 2 | 3 | 4 | 5 | 1 ")
